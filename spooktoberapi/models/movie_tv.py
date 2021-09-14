ab161c70ce9bfa964b613fd14a4657a7fa4586f2
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2021-09-14 15:46:27 -0500

    added movie_tv model

---
+++ spooktoberapi/models/movie_tv.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Movie_Tv(models.Model):
+    """Movie_Tv Model
+    Fields:
+    
+    """
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=150)
+    subGenre = models.CharField(max_length=150)
+    spirit = models.CharField(max_length=150)
+    era = models.CharField(max_length=150)
+    isMovie = models.BooleanField()
+    isNostalgic = models.BooleanField()
+    isMagic = models.BooleanField()
+    isTimBurton = models.BooleanField()
+    isStopMotion = models.BooleanField()
+    imdb_img = models.ImageField()
+    imdb_rating = models.IntegerField()
