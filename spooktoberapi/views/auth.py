3c1c4f13f2356a555f1fb897f59803b7ddcf65a7
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2021-09-20 09:09:47 -0500

    fixed new user issue

--- spooktoberapi/views/auth.py
+++ spooktoberapi/views/auth.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from spooktoberapi.models.spooktober_user import SpooktoberUser
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -31,11 +32,16 @@ def login(request):
             username=name,
         )
 
-        token = Token.objects.create(user_id=user.id)
+        spooktober_user = SpooktoberUser.objects.create(
+            user=user
+        )
+
+        token = Token.objects.create(user_id=spooktober_user.user.id)
         data = {
             'valid': True, 
-            'token': token.key 
+            'token': token.key
         }
+        
         return Response(data)
     
     except Exception as ex:
