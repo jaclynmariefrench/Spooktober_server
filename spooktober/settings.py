5384cd27264c72a6de8a9d2b8e9dcf157abd6171
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2021-10-05 10:13:59 -0500

    updated secret key situation

--- spooktober/settings.py
+++ spooktober/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,11 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ne)!6a*maki+2dihyily*jg$xg02o32#r7y5f#8=k3k*uogkr'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
